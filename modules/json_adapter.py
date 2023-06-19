e7cdc24b633b21271422281302e8a9ed9e1a38e3
metro <metrolog_omsk@mail.ru>
2023-06-19 18:28:08 +0500

    +Функция открытия файла диалогом, 100% готовности по ТЗ

--- modules/json_adapter.py
+++ modules/json_adapter.py
@@ -4,7 +4,7 @@ from modules import config
 
 def open_json() -> dict:
     my_dict = {}
-    with open(f'{config.FILEPATH}\data\data.json', 'r', encoding="utf_8") as file:
+    with open(f'{config.FILEPATH}', 'r', encoding="utf_8") as file:
         try:
             my_dict = json.loads(file.read())
         except:
@@ -14,6 +14,6 @@ def open_json() -> dict:
 
 
 def save_json(my_dict: dict):
-    with open(f'{config.FILEPATH}\data\data.json', 'w', encoding="utf_8") as file:
+    with open(f'{config.FILEPATH}', 'w', encoding="utf_8") as file:
         json.dump(my_dict, file)
         file.close()
